5808d7077ace70cc1dd90a322570d735cf8fb9ba
chaosh-liang <chao.liang.fly@gmail.com>
2021-02-13 20:33:05 +0800

    remove react from import statment

--- src/components/common/useDebounce.tsx
+++ src/components/common/useDebounce.tsx
@@ -1,4 +1,4 @@
-import react, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 export const useDebounce = (params: any, delay = 400) => {
   const [debouncedValue, setDebouncedValue] = useState(params);
